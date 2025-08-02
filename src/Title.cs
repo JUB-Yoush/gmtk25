3dd8c329bb7fff014b7f1acc14e81650ec88b108
JUB_Yoush <yoush1337@gmail.com>
2025-08-01 22:35:50 -0400

    new titlescreen

--- src/Title.cs
+++ src/Title.cs
@@ -10,12 +10,22 @@ using Raylib_cs;
 
 public static class TitleHandler
 {
-    public static readonly Rectangle StartBtnBox = new(244, 301, 272, 112);
+    public static int phoneFrame = 0;
+    public static bool hoverOnPhone = false;
+    public static readonly Rectangle StartBtnBox = new(111, 55, 320, 255);
+
+    public static int frame = 0;
+    public static double frametimer = 0;
+    public const int ANIM_FRAMES = 3;
+    public const int ENTITY_TILE_SIZE = 16;
+    public const int FPS = 10;
 
     public static void Update()
     {
         Vec2 mousePos = Raylib.GetMousePosition();
         Rectangle mouseHbox = new(mousePos.X, mousePos.Y, 4, 4);
+
+        hoverOnPhone = Raylib.CheckCollisionRecs(mouseHbox, StartBtnBox);
         if (
             Raylib.CheckCollisionRecs(mouseHbox, StartBtnBox)
             && Raylib.IsMouseButtonPressed(MouseButton.Left)
@@ -23,5 +33,14 @@ public static class TitleHandler
         {
             GlobalGameState.currentState = GameStates.INTRO;
         }
+
+        //animate phone
+        //
+        frametimer = JLib.Wrap(
+            frametimer + (FPS * Raylib.GetFrameTime()),
+            0,
+            Math.Pow(ANIM_FRAMES, 2.0)
+        );
+        phoneFrame = (int)JLib.Wrap(Math.Floor(frametimer / ANIM_FRAMES), 0, ANIM_FRAMES);
     }
 }
